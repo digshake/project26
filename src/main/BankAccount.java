dd6d37c35a567194e2d2d88e801dbb7ab59dad73
Doug Shook <dshook@wustl.edu>
2026-03-04 11:22:14 -0600

    BankAccount deposits implemented!

---
+++ src/main/BankAccount.java
@@ -0,0 +1,22 @@
+package main;
+
+public class BankAccount {
+
+    private double balance;
+
+    public BankAccount() {
+        this.balance = 0;
+    }
+
+    public void deposit(double amount) {
+        if(amount > 0) {
+            this.balance += amount;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
